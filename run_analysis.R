bf71ab99a3bb96ac41f8de10e5a266201f1eb1bc
Randy Schmidt <me@r38y.com>
2017-06-11 13:45:03 -0500

    Start to load in data

--- run_analysis.R
+++ run_analysis.R
@@ -1,3 +1,5 @@
+# Download and unzip data
+
 zipFile <- "./UCI_HAR_Dataset.zip"
 unzipDir <- "./data"
 if (!file.exists(zipFile) & !file.exists(unzipDir)) {
@@ -10,3 +12,27 @@ if (!file.exists(unzipDir)) {
 if (file.exists(zipFile)) {
   file.remove(zipFile)
 }
+
+loadData <- function(filename) {
+  fread(paste("./data/UCI HAR Dataset/", filename, sep = ""))
+}
+
+# 
+activityLabels <- loadData("activity_labels.txt") # 6 rows
+colNames <- loadData("features.txt") # 561 rows
+
+# Train Data
+
+trainActivities <- loadData("train/y_train.txt")
+trainSubjectNumbers <- loadData("train/subject_train.txt")
+trainReadings <- loadData("train/X_train.txt") # 561 cols
+
+# Test Data
+
+testActivities <- loadData("test/y_test.txt")
+testSubjectNumbers <- loadData("test/subject_test.txt")
+testReadings <- loadData("test/X_test.txt") # 561 cols
+
+# Merge the data
+
+# Write the new data set to disk
