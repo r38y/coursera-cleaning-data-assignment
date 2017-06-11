37c937090dfb134b67b0721aaaa7ef899bacc312
Randy Schmidt <me@r38y.com>
2017-06-11 12:39:17 -0500

    Downloading works

---
+++ run_analysis.R
@@ -0,0 +1,13 @@
+if(!file.exists("./data")) { dir.create("./data")}
+zipFile <- "./data/UCI_HAR_Dataset.zip"
+unzipDir <- "./data/UCI_HAR_Dataset"
+if (!file.exists(zipFile) & !file.exists(unzipDir)) {
+  fileURL <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
+  download.file(fileURL, destfile = zipFile, method = "curl")
+}
+if (!file.exists(unzipDir)) {
+  unzip(localFile, exdir = unzipDir)  
+}
+if (file.exists(zipFile)) {
+  file.remove(zipFile)
+}
