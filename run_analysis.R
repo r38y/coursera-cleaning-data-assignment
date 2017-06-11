147ea700c5eb63f05315394847e281f349f83732
Randy Schmidt <me@r38y.com>
2017-06-11 14:06:47 -0500

    Start to construct the tables

--- run_analysis.R
+++ run_analysis.R
@@ -13,26 +13,35 @@ if (file.exists(zipFile)) {
   file.remove(zipFile)
 }
 
+# Helper functions
 loadData <- function(filename) {
   fread(paste("./data/UCI HAR Dataset/", filename, sep = ""))
 }
 
-# 
+# Label data
 activityLabels <- loadData("activity_labels.txt") # 6 rows
 colNames <- loadData("features.txt") # 561 rows
 
-# Train Data
+# Train data
 
 trainActivities <- loadData("train/y_train.txt")
 trainSubjectNumbers <- loadData("train/subject_train.txt")
 trainReadings <- loadData("train/X_train.txt") # 561 cols
+# add headings
+names(trainReadings) <- as.vector(colNames[, V2])
 
-# Test Data
+# add activity
+trainReadings[, activity:=as.vector(trainActivities[, V1])]
+
+# add subject
+trainReadings[, subject:=as.vector(trainSubjectNumbers[, V1])]
+
+# Test data
 
 testActivities <- loadData("test/y_test.txt")
 testSubjectNumbers <- loadData("test/subject_test.txt")
 testReadings <- loadData("test/X_test.txt") # 561 cols
 
-# Merge the data
+# select columns
 
 # Write the new data set to disk
