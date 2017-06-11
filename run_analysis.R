a5d84c08e93c4e316a81f84017f1b803b32f8a45
Randy Schmidt <me@r38y.com>
2017-06-11 12:42:37 -0500

    Cleanup

--- run_analysis.R
+++ run_analysis.R
@@ -1,12 +1,11 @@
-if(!file.exists("./data")) { dir.create("./data")}
-zipFile <- "./data/UCI_HAR_Dataset.zip"
-unzipDir <- "./data/UCI_HAR_Dataset"
+zipFile <- "./UCI_HAR_Dataset.zip"
+unzipDir <- "./data"
 if (!file.exists(zipFile) & !file.exists(unzipDir)) {
   fileURL <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
   download.file(fileURL, destfile = zipFile, method = "curl")
 }
 if (!file.exists(unzipDir)) {
-  unzip(localFile, exdir = unzipDir)  
+  unzip(zipFile, exdir = unzipDir)  
 }
 if (file.exists(zipFile)) {
   file.remove(zipFile)
