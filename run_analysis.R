ad89849aed746ec10c8ba9d51e398cfdf7b3491f
Randy Schmidt <me@r38y.com>
2017-06-11 21:58:19 -0500

    It's starting to work

--- run_analysis.R
+++ run_analysis.R
@@ -1,68 +1,24 @@
 # The results from running this script will be saved in ./output
-# Misc helper functions are defined at the bottom of the file
 
-# Download and unzip data
-
-downloadData()
-
-# Label data
-
-activityLabels <- loadData("activity_labels.txt")
-activityLabels[,name:= lapply(gsub("_", " ", V2), tolower)]
-colNames <- loadData("features.txt")
-colNames <- as.vector(colNames[, V2])
-
-# Train data
-
-trainActivities <- loadData("train/y_train.txt")
-trainSubjectNumbers <- loadData("train/subject_train.txt")
-trainReadings <- loadData("train/X_train.txt")
-
-trainReadings <- mergeData(trainReadings, trainSubjectNumbers, trainActivities)
-
-# Test data
-
-testActivities <- loadData("test/y_test.txt")
-testSubjectNumbers <- loadData("test/subject_test.txt")
-testReadings <- loadData("test/X_test.txt")
-
-testReadings <- mergeData(testReadings, testSubjectNumbers, testActivities)
-
-# Combine the data
-readings <- rbind(trainReadings, testReadings)
-
-# Subset to just mean and std
-readings <- subset(readings, select = grep("[a-zA-Z]+-(std|mean)\\(\\).*|subject|activity", names(readings)))
-
-# Clean up column names
-names(readings) <- gsub("-", "_", names(readings))
-names(readings) <- gsub("\\(\\)", "", names(readings))
-names(readings) <- gsub("\\(\\)", "", names(readings))
-names(readings) <- gsub("^t{1}", "time_", names(readings))
-names(readings) <- gsub("^f{1}", "frequency_", names(readings))
-
-# Write the new data set to disk
-
-writeDataTable(readings, "readings.csv")
+library(dplyr)
 
 # Helper functions
 
 loadData <- function(filename) {
-  fread(paste("./data/UCI HAR Dataset/", filename, sep = ""))
+  read.table(paste("./data/UCI HAR Dataset/", filename, sep = ""))
 }
 
-mergeData <- function(readings, subjectNumbers, activities) {
+mergeData <- function(readings, subjectNumbers, activities, variableNames, activityLabels) {
   # add headings
-  names(readings) <- colNames
+  names(readings) <- variableNames
+  names(activities) <- c("activity_id")
+  names(subjectNumbers) <- c("subject")
   
-  # add activity
-  activityNames <- activityLabels[as.vector(activities[, V1]), name]
-  readings[, activity:=activityNames]
-  
-  # add subject
-  readings[, subject:=as.vector(subjectNumbers[, V1])]
-  # setcolorder(trainReadings, c("subject", "activity", colNames))
-  readings
+  # put pretty activity names as values
+  activities <- select(left_join(activities, activityLabels, by = "activity_id"), activity)
+
+  # combine them
+  cbind(subjectNumbers, activities, readings)
 }
 
 downloadData <- function() {
@@ -83,5 +39,49 @@ downloadData <- function() {
 writeDataTable <- function(dt, filename) {
   if(!file.exists("./output")) { dir.create("./output")}
   path <- paste("./output", filename, sep = "/")
-  fwrite(dt, file = path)
+  write.csv(dt, file = path)
 }
+
+# Download and unzip data
+
+downloadData()
+
+# Label data
+
+activityLabels <- loadData("activity_labels.txt")
+names(activityLabels) <- c("activity_id", "old_name")
+activityLabels <- mutate(activityLabels, activity = tolower(old_name))
+variableNames <- loadData("features.txt")$V2
+
+# Train data
+
+trainActivities <- loadData("train/y_train.txt")
+trainSubjectNumbers <- loadData("train/subject_train.txt")
+trainReadings <- loadData("train/X_train.txt")
+
+trainReadings <- mergeData(trainReadings, trainSubjectNumbers, trainActivities, variableNames, activityLabels)
+
+# Test data
+
+testActivities <- loadData("test/y_test.txt")
+testSubjectNumbers <- loadData("test/subject_test.txt")
+testReadings <- loadData("test/X_test.txt")
+
+testReadings <- mergeData(testReadings, testSubjectNumbers, testActivities, variableNames, activityLabels)
+
+# Combine the data
+readings <- rbind(trainReadings, testReadings)
+
+# Subset to just mean and std
+# readings <- subset(readings, select = grep("[a-zA-Z]+-(std|mean)\\(\\).*|subject|activity", names(readings)))
+
+# Clean up column names
+# names(readings) <- gsub("-", "_", names(readings))
+# names(readings) <- gsub("\\(\\)", "", names(readings))
+# names(readings) <- gsub("\\(\\)", "", names(readings))
+# names(readings) <- gsub("^t{1}", "time_", names(readings))
+# names(readings) <- gsub("^f{1}", "frequency_", names(readings))
+
+# Write the new data set to disk
+
+# writeDataTable(readings, "readings.csv")
